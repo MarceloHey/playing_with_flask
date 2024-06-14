244fb94f8c7bdbb0627f72abc3901fe2a941b327
MarceloHey <marcelotoniolo.h@gmail.com>
2024-06-13 23:03:29 -0300

    refactor: improve code readability and encapsulation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from blueprints import user_data_bp
 
 DB_FOLDER = "db"
 
@@ -6,6 +7,4 @@ app = Flask(__name__)
 app.config["DB_FOLDER"] = DB_FOLDER
 app.secret_key = "supersecretkey"
 
-from blueprints import user_data
-
-app.register_blueprint(user_data.__name__)
+app.register_blueprint(user_data_bp.__name__)
--- blueprints/user_data_bp.py
+++ blueprints/user_data_bp.py
@@ -18,11 +18,12 @@ def check_file_existence(path):
             print("File is NOT empty")
             return True
     except FileNotFoundError as e:
-        print("File NOT found")
+        print(f"File NOT found: {e}")
         return False
 
 
 def endcode_base_64(file):
+    """converte arquivo para base64"""
     try:
         base64_encoded = base64.b64encode(file)
         return base64_encoded.decode("utf-8")
@@ -31,9 +32,10 @@ def endcode_base_64(file):
 
 
 def write_user_data(json_data):
+    """escreve registro no arquivo no db.json"""
     file_path = f"{current_app.config['DB_FOLDER']}/db.json"
     try:
-        if check_file_existence(file_path) is False:
+        if not check_file_existence(file_path):
             # inicializa arquivo caso vazio ou inexistente
             with open(file_path, "w", encoding="utf-8") as file_db:
                 file_db.write(json.dumps({"user_data": []}))
@@ -51,32 +53,23 @@ def write_user_data(json_data):
         flash("Error on saving data")
 
 
-@__name__.route("/", methods=["GET", "POST"])
-def handle_form():
-    def render():
-        return render_template("main.html")
+def save_user_data(request):
+    error = None
+    success = None
+    user_data = {"name": None, "age": None, "photo": None}
 
-    def save_data():
-        error = None
-        success = None
-        user_data = {"name": None, "age": None, "photo": None}
-
-        if "photo" not in request.files:
-            flash("sem arquivo")
-            redirect(request.url)
-
-        req_file = request.files["photo"]
-
-        if req_file.filename == "":
-            flash("Nenhum arquivo selecionado")
-            return redirect(request.url)
-
+    if "photo" not in request.files:
+        flash("sem arquivo")
+        redirect(request.url)
+    elif request.files["photo"] == "":
+        flash("Nenhum arquivo selecionado")
+        return redirect(request.url)
+    else:
         try:
             user_data["name"] = request.form["name"]
             user_data["age"] = request.form["age"]
-            user_data["photo"] = endcode_base_64(req_file.read())
+            user_data["photo"] = endcode_base_64(request.files["photo"].read())
 
-            # escreve registro no arquivo no db.json
             write_user_data(user_data)
 
             success = "DADOSSSS!!! 😋😋😋"
@@ -86,7 +79,10 @@ def handle_form():
         finally:
             return render_template("main.html", error=error, success=success)
 
+
+@__name__.route("/", methods=["GET", "POST"])
+def handle_form():
     if request.method == "GET":
-        return render()
+        return render_template("main.html")
     if request.method == "POST":
-        return save_data()
+        return save_user_data(request)
